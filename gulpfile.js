1e9740e010ca6c6ed4cf1e12eb8da35ee4ce5314
heiligster <heiligster@googlemail.com>
2015-10-03 19:55:02 +0200

    Make things happen

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+/**
+ * -----------------------------------------------------------------------------
+ * @copyright 2015
+ * @author Philipp Müller
+ * @package
+ * @description
+ * Basic gulp setup
+ * -----------------------------------------------------------------------------
+ */
+var gulp    = require('gulp');
+var changed = require('gulp-changed');
+
+
+/**
+ * Define tasks to run via gulp.watch
+ */
+gulp.task('default', []);
+
+
+/**
+ * Gets called whenever gulp recongnizes file changes
+ */
+gulp.task('watch', function () {
+  
+});
