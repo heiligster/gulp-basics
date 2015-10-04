92eda0abd64b1804f6fdef08a1083ae18bc65896
heiligster <heiligster@googlemail.com>
2015-10-04 16:09:36 +0200

    css task

--- gulpfile.js
+++ gulpfile.js
@@ -7,19 +7,51 @@
  * Basic gulp setup
  * -----------------------------------------------------------------------------
  */
+
+/*@const----------------------------------------------------------------------*/
+var BASE_PATH    = __dirname + '/';
+var CSS_SRC      = BASE_PATH + 'src/**/*.scss';
+var CSS_DEST     = BASE_PATH + 'dest/';
+var CSS_OUTFILE  = 'application.css';
+
+/*@deps-----------------------------------------------------------------------*/
 var gulp    = require('gulp');
+var util    = require('gulp-util');
 var changed = require('gulp-changed');
+var rename  = require('gulp-rename');
 
+var postcss    = require('gulp-postcss');
+var prefixer   = require('autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var sass       = require('gulp-sass');
+var nano       = require('cssnano');
 
+/*@funcs----------------------------------------------------------------------*/
 /**
  * Define tasks to run via gulp.watch
  */
-gulp.task('default', []);
+gulp.task('default', ['css', 'watch']);
+
+
+/**
+ * Run preprocessors and compile css
+ */
+gulp.task('css', function () {
+  return gulp.src(CSS_SRC)
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      includePaths: ['./src/partials']
+    }))
+    .pipe(postcss([prefixer, nano]))
+    .pipe(rename(CSS_OUTFILE))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(CSS_DEST));
+});
 
 
 /**
  * Gets called whenever gulp recongnizes file changes
  */
 gulp.task('watch', function () {
-  
+  gulp.watch(CSS_SRC, ['css']);
 });
