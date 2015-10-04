a90461231eeb03f3cb3b3ad930f654e6e2a2acdc
heiligster <heiligster@googlemail.com>
2015-10-04 16:57:53 +0200

    sync task added

--- gulpfile.js
+++ gulpfile.js
@@ -9,10 +9,10 @@
  */
 
 /*@const----------------------------------------------------------------------*/
-var BASE_PATH    = __dirname + '/';
-var CSS_SRC      = BASE_PATH + 'src/**/*.scss';
-var CSS_DEST     = BASE_PATH + 'dest/';
-var CSS_OUTFILE  = 'application.css';
+var BASE_PATH   = __dirname + '/';
+var CSS_SRC     = BASE_PATH + 'src/**/*.scss';
+var CSS_DEST    = BASE_PATH + 'dest/';
+var CSS_OUTFILE = 'application.css';
 
 /*@deps-----------------------------------------------------------------------*/
 var gulp    = require('gulp');
@@ -25,6 +25,8 @@ var prefixer   = require('autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var sass       = require('gulp-sass');
 var nano       = require('cssnano');
+
+var sync = require('browser-sync').create();
 
 /*@funcs----------------------------------------------------------------------*/
 /**
@@ -45,7 +47,25 @@ gulp.task('css', function () {
     .pipe(postcss([prefixer, nano]))
     .pipe(rename(CSS_OUTFILE))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(CSS_DEST));
+    .pipe(gulp.dest(CSS_DEST))
+    .pipe(sync.stream());
+});
+
+
+/**
+ * Sync file changes and reload browser accordingly
+ */
+gulp.task('sync', function () {
+  var options = {
+    proxy: 'gulp-basics.local',
+    injectChanges: true,
+    notify: false
+  };
+
+  sync.init(options);
+
+  gulp.watch(CSS_SRC, ['css']);
+  gulp.watch('*.html').on('change', sync.reload);
 });
 
 
